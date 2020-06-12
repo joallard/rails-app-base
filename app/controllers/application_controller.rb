d9abeebeca265acd1a374049bf63e2deb7f62a0d
Jonathan Allard <jonathan@allard.io>
2020-06-12 04:29:44 -0400

    AppCtlr: Add #parameter_missing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+
+  rescue_from ActionController::ParameterMissing, with: :parameter_missing
+
+  protected
+
+  def parameter_missing(ex)
+    render status: 400, json: {errors: [
+      {name: :parameter_missing, message: ex.message}
+    ]}
+  end
 end
